c127071e97c550a61441a8df5fe8048d8fffc336
yehonadav <yonadav.barilan@gmail.com>
2019-11-13 16:38:08 +0200

    build candidate 2019.11.13.14.38.8.658570

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     with open("README.md", encoding="utf8") as f: long_description = f.read()
     setup(
         name=package_name,
-        version="0.0.1",
+        version="2019.11.13.14.38.8.658570",
         author="yehonadav",
         author_email="qaviton@gmail.com",
         description="qaviton log",
